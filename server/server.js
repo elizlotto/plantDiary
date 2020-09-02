bbf3c08e47730d6807a598ec942d3498fc429d34
Elizabeth Lotto <elizlotto@gmail.com>
2020-09-02 13:37:04 -0400

    server and mongo db connected

---
+++ server/server.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const path = require('path');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+//conect to mongoDB
+const DB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.novem.mongodb.net/Plantery?retryWrites=true&w=majority`;
+
+mongoose.connect(DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//connecting to DB
+mongoose.connection.once('open', () => {
+  console.log('Connected to database.');
+});
+
+// JSON parser:
+app.use(express.json());
+
+// Webpack DevServer
+if (process.env.NODE_ENV === 'production') {
+  // statically serve everything in the dist folder on the route
+  app.use('/dist', express.static(path.resolve(process.cwd(), './dist')));
+  // serve index.html on the route '/'
+  app.get('/', (req, res) => {
+    res.status(200).sendFile(path.resolve(process.cwd(), './client/src/index.html'));
+  });
+}
+
+// catch-all endpoint handler
+app.use((req, res) => {
+  return res.status(400).send('Page not found.')
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unkown middleware error!',
+    status: 500,
+    message: { err: 'An error occurred!' }
+  };
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+})
+
+app.listen(PORT, () => {
+  console.log('Magic happening on ' + PORT);
+});
+
+module.exports = app;
