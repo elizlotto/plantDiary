cbff532ff153f2750e92230bf56756b66321e14c
Elizabeth Lotto <elizlotto@gmail.com>
2020-10-01 23:02:34 -0400

    moved mongoose connect to schema file

--- server/controller/plantController.js
+++ server/controller/plantController.js
@@ -1,46 +1,55 @@
-const Plant = require('../models/plantSchema');
+const Plant = require("../models/plantSchema");
+const mongoose = require('mongoose');
 
 module.exports = {
+  createPlant(req, res, next) {
+    console.log(req.body, 'req')
+    console.log("in plantController create");
+    const { name, petName, acquired, status, price } = req.body;
 
-    createPlant(req, res, next) {
-        console.log('in plantController create')
-        const { name, petName, acquired, status, price } = req.body;
-        if (!name) return res.status(400).json("Missing plant name!");
-        Plant.create({ name, petName, acquired, status, price }, (err, newPlant) => {
-            if (err) return next(err);
-            res.locals.plant = newPlant;
-            return next();
-        });
-    },
+    if (!name) return res.status(400).json("Missing plant name!");
+    Plant.create(
+      { name, petName, acquired, status, price },
+      (err, newPlant) => {
+        if (err) return next(err);
+        res.locals.plant = newPlant;
+        return next();
+      }
+    );
+  },
 
-    getPlants(req, res, next) {
-        Plant.find({}, (err, allPlants) => {
-            if (err) return next(err);
-            res.locals.plant = allPlants;
-            return next();
-        });
-    },
-   
-    editPlant(req, res, next) {
-        const { _id } = req.params;
-        const { status, petName } = req.body;
-        if (!{ status }) return res.status(400).json("Missing an update!");
-        Plant.findOneAndUpdate({ _id }, { status, petName }, { new: true }, (err, editedPlant) => {
-            if (err) return next(err);
-            res.locals.plant = editedPlant;
-            return next();
-        });
-    },
+  getPlants(req, res, next) {
+    Plant.find({}, (err, allPlants) => {
+      if (err) return next(err);
+      res.locals.plant = allPlants;
+      return next();
+    });
+  },
 
-    deletePlant(req, res, next){
-        const { _id } = req.params;
-        if (!_id) return res.status(400).json("Missing information");
-        Plant.findOneAndDelete({ _id }, (err, deleted) => {
-            if (err) return next(err);
-            res.locals.plant = deleted;
-            return next();
-        });
-    }
+  editPlant(req, res, next) {
+    console.log('in edit')
+    const { _id } = req.params;
+    const { status, petName } = req.body;
+    if (!status) return res.status(400).json("Missing a status!");
+    Plant.findOneAndUpdate(
+      { _id },
+      { status, petName },
+      { new: true },
+      (err, editedPlant) => {
+        if (err) return next(err);
+        res.locals.plant = editedPlant;
+        return next();
+      }
+    );
+  },
 
-}
-
+  deletePlant(req, res, next) {
+    const { _id } = req.params;
+    if (!_id) return res.status(400).json("Missing information");
+    Plant.findOneAndDelete({ _id }, (err, deleted) => {
+      if (err) return next(err);
+      res.locals.plant = deleted;
+      return next();
+    });
+  },
+};
--- server/server.js
+++ server/server.js
@@ -1,46 +1,31 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const path = require('path');
-const dotenv = require('dotenv');
-dotenv.config();
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const plantController = require('./controller/plantController');
+const path = require("path");
 
-//conect to mongoDB
-const DB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.novem.mongodb.net/Plantery?retryWrites=true&w=majority`;
-
-mongoose.connect(DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-//connecting to DB
-mongoose.connection.once('open', () => {
-  console.log('Connected to database.');
-});
+const cookieParser = require("cookie-parser");
+const plantController = require("./controller/plantController");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/plant', plantController.getPlants, (req, res) => {
-  console.log('get plants')
+
+app.get("/plant", plantController.getPlants, (req, res) => {
   res.json(res.locals.plant);
 });
 
-app.post('/plant', plantController.createPlant, (req, res) => {
-  console.log('in plant post route')
+app.post("/plant", plantController.createPlant, (req, res) => {
   res.json(res.locals.plant);
 });
 
-app.put('/plant/:_id', plantController.editPlant, (req, res) => {
+app.put("/plant/:_id", plantController.editPlant, (req, res) => {
+  console.log('in put')
   res.json(res.locals.plant);
 });
 
-app.delete('/plant/:_id', plantController.deletePlant, (req, res) => {
+app.delete("/plant/:_id", plantController.deletePlant, (req, res) => {
+  console.log('in delete')
   res.json(res.locals.plant);
 });
 
@@ -52,17 +37,17 @@ app.delete('/plant/:_id', plantController.deletePlant, (req, res) => {
 // global error handler
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unkown middleware error!',
+    log: "Express error handler caught unkown middleware error!",
     status: 500,
-    message: { err: 'An error occurred!' }
+    message: { err: "An error occurred!" },
   };
   const errorObj = Object.assign(defaultErr, err);
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
-})
+});
 
 app.listen(PORT, () => {
-  console.log('Magic happening on ' + PORT);
+  console.log("Magic happening on " + PORT);
 });
 
 module.exports = app;
