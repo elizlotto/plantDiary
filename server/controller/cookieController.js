c7eef8e1b1769fa8bb97a5e3eb40ac96e96d4bc2
Elizabeth Lotto <elizlotto@gmail.com>
2020-10-06 21:36:12 -0400

    added cookie controller and additonal express routes and middleware

--- server/controller/cookieController.js
+++ server/controller/cookieController.js
@@ -1,9 +1,17 @@
 module.exports = {
   setSSIDcookie(req, res, next) {
-    
+    res.cookie("user", res.locals.token, { httpOnly: true });
+  const { name } = res.locals.person;
+  res.cookie("name", name, { httpOnly: false });
+  console.log(name);
+  // decodeURIComponent('shmaryeh%40gmail.com')
+  return next();
   },
   
   verifyCookie(req, res, next) {
-    
+    if (req.cookies.user) {
+      return next();
+    }
+    return res.redirect("/");
   }
 }
--- server/server.js
+++ server/server.js
@@ -6,15 +6,34 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const plantController = require("./controller/plantController");
 const authController = require("./controller/authController");
-const { oAuth } = require("./controller/authController");
+const cookieController = require('./controller/cookieController');
+const jwtDecode = require("jwt-decode")
 
+
+require("dotenv").config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.get('/login', authController.oAuth, (req, res) => {
   return res.redirect(res.locals.url);
-})
+});
+
+app.get(
+  "/success",
+  authController.onSuccess,
+  // add middleware to see if user is in database, if not, add them
+  cookieController.setSSIDcookie,
+  (req, res) => {
+    // redirect to a page that will then check if there is an SSID cookie called 'user' ()
+    res.status(200).redirect("/home");
+  }
+);
+
+app.get("/home", cookieController.verifyCookie, (req, res) => {
+  // add middleware to check for SSID cookie
+  res.status(200).sendFile(path.resolve(__dirname, "../client/index.html"));
+});
 
 app.get("/plant", plantController.getPlants, (req, res) => {
   res.json(res.locals.plant);
