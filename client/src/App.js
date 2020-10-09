abf9b463a9ca81d389175d752124d9bd86ef56fc
Elizabeth Lotto <elizlotto@gmail.com>
2020-10-08 22:12:07 -0400

    note. need style loader scss etc for webpack to work

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from '../components/Login';
 import PlantContainer from '../components/PlantContainer';
 import './assets/styles.scss';
+//note must result webpack does not have style loader etc 
 
 const App = () => {
   return (
