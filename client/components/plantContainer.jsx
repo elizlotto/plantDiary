a897227c1611cc5bbe7daefc0a836bd03efe6c85
Elizabeth Lotto <elizlotto@gmail.com>
2020-11-04 23:44:43 -0500

    updated container

--- client/components/plantContainer.jsx
+++ client/components/plantContainer.jsx
@@ -35,21 +35,11 @@ const PlantContainer = () => {
   }, []);
  
   { console.log(allPlants, 'all line 27') }
-  // const plantData = allPlants.map(plant => {
 
-  // })
-//plant container is the redirect after login. 
-  //will render a plant card 
-  //will also fetch from the plantContainer
-  //use user obj to link database entries. 
   return (
    
     <div className="PlantContainer">
-      <ul>
-        {allPlants.map(plant => (
-          <PlantCard id={plant._id} name={plant.name} />
-        ))}
-       </ul>
+    */}
       <div>Welcome {user.name}!</div>
       <p>Plants coming soon!</p>
     </div>
